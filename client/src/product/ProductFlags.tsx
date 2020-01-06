c78e281bcf503cf6a0a737cda5dc3a7c1d729dee
Stu Sherwin <stu.sherwin@gmail.com>
2020-01-06 14:07:48 +0000

    Fix product flags styling

--- client/src/product/ProductFlags.tsx
+++ client/src/product/ProductFlags.tsx
@@ -11,10 +11,10 @@ export interface HasProductFlags { biodynamic: boolean
 
 export let ProductFlags = ({p}: {p: HasProductFlags}) => 
   <span>
-    <span className={classNames("inline-block mr-1 w-4 h-4 text-center nudge-d-2", { "text-white bg-grey": p.biodynamic, "text-grey": !p.biodynamic })}><span className="inline-block nudge-u-2">B</span></span>
-    <span className={classNames("inline-block mr-1 w-4 h-4 text-center nudge-d-2", { "text-white bg-grey": p.glutenFree, "text-grey": !p.glutenFree })}><span className="inline-block nudge-u-2">G</span></span>
-    <span className={classNames("inline-block mr-1 w-4 h-4 text-center nudge-d-2", { "text-white bg-grey": p.organic,    "text-grey": !p.organic    })}><span className="inline-block nudge-u-2">O</span></span>
-    <span className={classNames("inline-block mr-1 w-4 h-4 text-center nudge-d-2", { "text-white bg-grey": p.fairTrade,  "text-grey": !p.fairTrade  })}><span className="inline-block nudge-u-2">F</span></span>
-    <span className={classNames("inline-block mr-1 w-4 h-4 text-center nudge-d-2", { "text-white bg-grey": p.vegan,      "text-grey": !p.vegan      })}><span className="inline-block nudge-u-2">V</span></span>
-    <span className={classNames("inline-block mr-1 w-4 h-4 text-center nudge-d-2", { "text-white bg-grey": p.addedSugar, "text-grey": !p.addedSugar })}><span className="inline-block nudge-u-2">S</span></span>
+    <span className={classNames("inline-block mr-1 w-4 h-4 text-center", { "text-white bg-grey": p.biodynamic, "text-grey": !p.biodynamic })}><span className="inline-block">B</span></span>
+    <span className={classNames("inline-block mr-1 w-4 h-4 text-center", { "text-white bg-grey": p.glutenFree, "text-grey": !p.glutenFree })}><span className="inline-block">G</span></span>
+    <span className={classNames("inline-block mr-1 w-4 h-4 text-center", { "text-white bg-grey": p.organic,    "text-grey": !p.organic    })}><span className="inline-block">O</span></span>
+    <span className={classNames("inline-block mr-1 w-4 h-4 text-center", { "text-white bg-grey": p.fairTrade,  "text-grey": !p.fairTrade  })}><span className="inline-block">F</span></span>
+    <span className={classNames("inline-block mr-1 w-4 h-4 text-center", { "text-white bg-grey": p.vegan,      "text-grey": !p.vegan      })}><span className="inline-block">V</span></span>
+    <span className={classNames("inline-block mr-1 w-4 h-4 text-center", { "text-white bg-grey": p.addedSugar, "text-grey": !p.addedSugar })}><span className="inline-block">S</span></span>
   </span>
