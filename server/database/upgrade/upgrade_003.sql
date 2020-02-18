5a5695b1d8508ead57467cfbcafde4deeb9e7dc2
Stu Sherwin <stu.sherwin@gmail.com>
2020-02-18 05:34:00 +0000

    Fix upgrade script

--- server/database/upgrade/upgrade_003.sql
+++ server/database/upgrade/upgrade_003.sql
@@ -1,7 +1,4 @@
-CREATE TABLE
-GRANT
-GRANT
-stment
+create table order_item_adjustment
 ( order_id                  int  not null
 , household_id              int  not null
 , product_id                int  not null
@@ -14,14 +11,4 @@ stment
 , primary key (order_id, household_id, product_id)
 , foreign key (order_id, household_id, product_id) references past_household_order_item (order_id, household_id, product_id)
 , foreign key (order_group_id) references order_group (id)
-)
-
--- create function int_sum_null(int, int)
--- returns int language sql as $$
---     select $1 + $2
--- $$;
-
--- create aggregate sumnull(integer) (
---     sfunc = int_sum_null,
---     stype = int
--- );
+);
