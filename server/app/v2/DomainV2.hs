f6d4c49c1ced0af9597d50bd1700002a34e45307
Stu Sherwin <stu.sherwin@gmail.com>
2020-04-09 21:51:44 +0100

    wip

--- server/app/v2/DomainV2.hs
+++ server/app/v2/DomainV2.hs
@@ -44,10 +44,8 @@ household :: HouseholdInfo -> Maybe String -> Maybe String -> Maybe String -> [H
 household householdInfo contactName contactEmail contactPhone householdOrders payments =
   Household householdInfo contactName contactEmail contactPhone totalOrders totalPayments balance
   where
-  totalOrders = _incVat $ sum . map (\ho -> case ho of 
-    (HouseholdOrder { _householdOrderAdjustment = Just adj }) -> adj
-    _ -> _householdOrderTotal ho) $ filter (not . isHouseholdOrderAbandoned) householdOrders
-  totalPayments = sum . map _paymentAmount $ payments
+  totalOrders = _incVat $ sum . map (\ho -> fromMaybe (_householdOrderTotal ho) (fmap _orderAdjNewTotal . _householdOrderAdjustment $ ho)) $ filter (not . isHouseholdOrderAbandoned) householdOrders
+  totalPayments = sum . map _paymentAmount $ payments 
   balance = totalPayments - totalOrders
 
 {- Payment -}
